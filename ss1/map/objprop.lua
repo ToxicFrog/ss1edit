a3fb269eff4b235e683cdc1f945dccf2b81a408e
Ben Kelly <bk@ancilla.ca>
2014-07-11 21:47:10 -0400

    Read additional object properties

--- ss1/map/objprop.lua
+++ ss1/map/objprop.lua
@@ -1,31 +1,20 @@
 local vstruct = require "vstruct"
+local formats = require "ss1.map.objprop.formats"
 
 -- offset from the base chunk ID of the level object table
 local OFFSET = 8
 
--- format of a common object instance properties struct
-local MAP_OBJECTS = [[
-  %d * {
-    used:b1
-    class:u1
-    subclass:u1
-    info_index:u2
-    xref_index:u2
-    prev:u2
-    next:u2
-    x:u2
-    y:u2
-    z:u1
-    pitch:u1
-    yaw:u1
-    roll:u1
-    ai_maybe:u1
-    type:u1
-    hp_maybe:u2
-    state:u1
-    unknown:s3
-  }
-]]
+local typeloader = {}
+
+typeloader[13] = function(objects, object)
+  local contents = object.contents
+  object.contents = {}
+  for _,obj in ipairs(contents) do
+    if objects[obj].used then
+      table.insert(object.contents, objects[obj])
+    end
+  end
+end
 
 local function load(self)
   -- to fully realize an object, we need to read its information from five places
@@ -38,8 +27,34 @@ local function load(self)
   -- I'm not sure what a non-terrible API for this looks like.
 
   local buf = self.res:get(self.id + OFFSET).data
-  assert(#buf % 27 == 0, "confusing object table length")
-  return vstruct.read(MAP_OBJECTS % (#buf/27), buf)
+  local objects = {}
+  local details = {}
+
+  for class=0,14 do
+    if formats[class] then
+      details[class] = vstruct.array(
+          formats[class],
+          self.res:get(self.id + 10 + class).data, 0)
+    --else
+    --  print("No format definition available for class %d (%d bytes)" % {
+    --    class, self.res:get(self.id + 10 + class).size})
+    end
+  end
+
+  local raw_objects = vstruct.array(formats.Object, buf, 0)
+  for id,object in ipairs(raw_objects) do
+    if object.used then
+      table.insert(objects, object)
+      if details[object.class] then
+        table.merge(object, details[object.class][object.detail_index], "error")
+        if typeloader[object.class] then
+          typeloader[object.class](raw_objects, object)
+        end
+      end
+    end
+  end
+
+  return objects
 end
 
 return {
---
+++ ss1/map/objprop/formats.lua
@@ -0,0 +1,112 @@
+local formats = { details = {} }
+
+-- Common object properties.
+formats.Object = [[
+  used:b1
+  class:u1
+  subclass:u1
+  detail_index:u2
+  xref_index:u2
+  prev:u2
+  next:u2
+  x:u2
+  y:u2
+  z:u1
+  pitch:u1
+  yaw:u1
+  roll:u1
+  ai_maybe:u1
+  type:u1
+  hp_maybe:u2
+  state:u1
+  unknown:s3
+]]
+
+-- Weapons.
+formats[0] = [[
+  x6
+  ammo_type:u1
+  ammo_count:u1
+]]
+
+-- Ammo has no interesting properties.
+formats[1] = [[
+  x6
+]]
+
+-- Projectiles have not been reverse engineered and exist only in save games.
+-- I suspect it will contain at least vector information, but it may also
+-- contain damage information for variable-power projectiles like plasma bolts.
+
+-- Grenades and explosives have not been reverse engineered yet.
+
+-- Dermal patches have no interesting properties.
+formats[4] = [[
+  x6
+]]
+
+-- Hardware.
+formats[5] = [[
+  x6
+  version:u1
+]]
+
+
+-- Software, logs and notes.
+formats[6] = [[
+  x6
+  version:u1
+  log_id:u1
+  log_level:u1
+]]
+
+-- Scenery and decorations. Too complicated right now.
+formats[7] = [[
+  x16
+]]
+
+-- Inventory items. Not yet reverse engineered.
+
+-- Switches and panels.
+formats[9] = [[
+  x6
+  unknown_9:s2
+  condition_variable:u2
+  condition_message:u2
+  union:s18
+]]
+
+-- Doors and gratings.
+formats[10] = [[
+  x6
+  trigger:u2
+  message:u2
+  access:u1
+  unknown_10:s3
+]]
+
+-- Animations not yet reverse engineered.
+
+-- Traps and triggers.
+-- Too complicated to handle yet.
+formats[12] = [[
+  x28
+]]
+
+-- Containers.
+formats[13] = [[
+  x6
+  contents:{ 4*u2 }
+  width:u1
+  height:u1
+  depth:u1
+  texture:{
+    top:u1
+    side:u1
+  }
+  unknown_13:s2
+]]
+
+-- Critters not yet reverse engineered.
+
+return formats
