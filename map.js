02d4ca5aeb00311886663bb4aa9f57d318518780
Ben Kelly <bk@ancilla.ca>
2014-09-22 17:00:43 -0400

    Split map into separate js and html halves

---
+++ map.js
@@ -0,0 +1,15 @@
+var SCALE=${SCALE}; /* pixels per map tile */
+var WIDTH=${WIDTH};
+var HEIGHT=${HEIGHT};
+
+var tile_info = {
+  ${TILE_INFO}
+};
+
+var object_info = {
+  ${OBJECT_INFO}
+};
+
+function drawTerrain() {
+  ${WALLS}
+}
