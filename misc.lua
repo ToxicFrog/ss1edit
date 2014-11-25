95148b796f12367443f69fc0b716107656cd66e3
Ben Kelly <bk@ancilla.ca>
2014-11-24 20:48:10 -0500

    Implement lfs.normalize, lfs.dirname, and lfs.rmkdir; fix lfs.exists on windows
    It turns out that windows is full of spiders: stat() will fail if the path has
    a trailing directory, unless it's a root directory path, in which case it will
    fail if it *doesn't* have one. lfs.exists now contains an ugly workaround for
    this.
    
    lfs.dirname() works as expected, except that \ may be translated to / in the
    return value on windows.
    
    lfs.rmkdir() replicates the behaviour of 'mkdir -p', i.e. it creates missing
    intermediate directories and does not fail if the target already exists.

--- misc.lua
+++ misc.lua
@@ -57,7 +57,58 @@ function partial(f, ...)
 end
 
 if lfs then
+	local windows = package.config:sub(1,1) == "\\"
+
+	-- We make the simplifying assumption in these functions that path separators
+	-- are always forward slashes. This is true on *nix and *should* be true on
+	-- windows, but you can never tell what a user will put into a config file
+	-- somewhere. This function enforces this.
+	function lfs.normalize(path)
+		if windows then
+			return (path:gsub("\\", "/"))
+		else
+			return path
+		end
+	end
+
 	function lfs.exists(path)
+		path = lfs.normalize(path)
+		if windows then
+			-- Windows stat() is kind of awful. If the path has a trailing slash, it
+			-- will always fail. Except on drive root directories, which *require* a
+			-- trailing slash. Thankfully, appending a "." will always work.
+			path = path:gsub("/$", "/.")
+		end
+
 		return lfs.attributes(path, "mode") ~= nil
+	end
+
+	function lfs.dirname(oldpath)
+		local path = lfs.normalize(oldpath):gsub("[^/]+/*$", "")
+		if path == "" then
+			return oldpath
+		end
+		return path
+	end
+
+	-- Recursive directory creation a la mkdir -p. Unlike lfs.mkdir, this will
+	-- create missing intermediate directories, and will not fail if the
+	-- destination directory already exists.
+	-- It assumes that the directory separator is '/' and that the path is valid
+	-- for the OS it's running on, e.g. no trailing slashes on windows -- it's up
+	-- to the caller to ensure this!
+	function lfs.rmkdir(path)
+		if lfs.exists(path) then
+			return true
+		end
+		if lfs.dirname(path) == path then
+			-- We're being asked to create the root directory!
+			return nil,"mkdir: unable to create root directory"
+		end
+		local r,err = lfs.rmkdir(lfs.dirname(path))
+		if not r then
+			return nil,err.." (creating "..path..")"
+		end
+		return lfs.mkdir(path)
 	end
 end
