e9f2b53ef5338df2bbe195e1daa825d73b28f122
Ben Kelly <bk@ancilla.ca>
2015-08-01 07:51:29 -0400

    assert on failures in writefile/readfile

--- io.lua
+++ io.lua
@@ -15,14 +15,14 @@ end
 
 function io.readfile(name)
   local fd = assert(io.open(name, "rb"))
-  local buf = fd:read("*a")
+  local buf = assert(fd:read("*a"))
   fd:close()
   return buf
 end
 
 function io.writefile(name, data)
   local fd = assert(io.open(name, "wb"))
-  fd:write(data)
+  assert(fd:write(data))
   fd:close()
 end
 
